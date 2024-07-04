e6e2eec9a8566d2605503f330632d8bd1fddec43
Shafayat-Fahim <theanonto@outlook.com>
2024-07-04 23:33:26 +0600

    Practise02

---
+++ CSE213/src/main/java/com/cse/oop/courseprojects/Practise02.java
@@ -0,0 +1,14 @@
+package com.cse.oop.courseprojects;
+
+public class Practise02 {
+    public static void main(String[] args) {
+        // variables
+        String name = "Anonto";
+        int age = 69;
+        String city = "Dhaka";
+        int favNum = age;
+
+        System.out.println(favNum);
+
+    }
+}
