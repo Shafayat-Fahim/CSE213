d5e2928de32853cac76207f8975bc5fd46212a3e
Shafayat-Fahim <theanonto@outlook.com>
2024-07-04 23:21:58 +0600

    Practise01

---
+++ CSE213/src/main/java/com/cse/oop/courseprojects/Practise01.java
@@ -0,0 +1,8 @@
+package com.cse.oop.courseprojects;
+
+public class Practise01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("Hello Java!");
+    }
+}
