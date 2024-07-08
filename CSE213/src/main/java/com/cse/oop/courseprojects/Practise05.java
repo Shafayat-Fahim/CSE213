830b0398b8664f3186acbc02ae1b31bbcc58eaa9
Shafayat-Fahim <theanonto@outlook.com>
2024-07-08 17:43:55 +0600

    Practise06

--- CSE213/src/main/java/com/cse/oop/courseprojects/Practise05.java
+++ CSE213/src/main/java/com/cse/oop/courseprojects/Practise05.java
@@ -35,7 +35,7 @@ public class Practise05 {
         int[] age = {34,65,35};
         System.out.println(age[2]);
 
-        
+
 
     }
 }
---
+++ CSE213/src/main/java/com/cse/oop/courseprojects/Practise06.java
@@ -0,0 +1,21 @@
+package com.cse.oop.courseprojects;
+
+import java.util.Arrays;
+
+public class Practise06 {
+    public static void main(String[] args) {
+        //
+        double price = 100.00;
+        double finalPrice = price + 17;
+        System.out.println(finalPrice);
+
+        int price2 = 100;
+        int finalPrice2 = price2 + 17;
+        System.out.println(finalPrice2);
+
+        int price3 = 100;
+        int finalPrice3 = price2 + (int)17.00;
+        System.out.println(finalPrice3);
+
+    }
+}
