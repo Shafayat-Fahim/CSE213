c63285e1f78a3d6fe9f9a0eb5be0ddcafa35e9d6
Shafayat-Fahim <theanonto@outlook.com>
2024-07-08 17:50:29 +0600

    Practise07

---
+++ CSE213/src/main/java/com/cse/oop/courseprojects/Practise07.java
@@ -0,0 +1,11 @@
+package com.cse.oop.courseprojects;
+
+import java.util.Arrays;
+
+public class Practise07 {
+    public static void main(String[] args) {
+        // constant
+        final float PI = 3.14F;
+        System.out.println(PI);
+    }
+}
